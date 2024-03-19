c812024b32e2a45f129634426e1b0cce10d08b80
leatherbullet <al.obnizov@yandex.ru>
2024-03-19 15:00:19 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4 };
+            int tempNumber;
+            int userInput;
+
+            foreach (int number in numbers)
+                Console.Write(number + " ");
+
+            Console.WriteLine("\nна сколько позиций влево хотите сместить?");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < userInput; i++)
+            {
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    tempNumber = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = tempNumber;
+                }
+            }
+
+            foreach (int number in numbers)
+                Console.Write(number + " ");
+        }
+    }
+}
